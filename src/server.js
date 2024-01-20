d37c9303838952fd78956dd6d93713e7bef274a0
dmiryMS <dm.zhukov7@mail.ru>
2024-01-20 21:19:48 +0300

    jira_with_auth

--- src/server.js
+++ src/server.js
@@ -14,34 +14,196 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logMiddleware);
 
 app.post('/track/start', async (req, res) => {
-  const result = await prisma.work_time.create({
-    data: {
-      begin_time: new Date().toISOString()
-    }
-  });
+  const token = req.headers['token'];
 
-  res.json(result);
+  try {
+    var decoded = jwt.verify(token, cfg.jwt.secret, {
+      ignoreExpiration: false
+    });
+
+    if (decoded) {
+      const id = jwt.decode(token, { complete: true });
+
+      const verify_user = await prisma.user.findFirst({
+        where: {
+          id: id.payload.id
+        }
+      });
+
+      if (verify_user) {
+        const verify_work = await prisma.work_time.findFirst({
+          where: {
+            user_id: id.payload.id,
+            end_date: null
+          }
+        });
+
+        if (verify_work === null) {
+          const result = await prisma.work_time.create({
+            data: {
+              begin_date: new Date().toISOString(),
+              user_id: verify_user.id
+            }
+          });
+
+          return res.status(200).json(result);
+        }
+
+        return res.status(200).json({ message: 'Есть незаконченная работа!' });
+      }
+    }
+
+    return res.status(401).json({});
+  } catch (err) {
+    err = {
+      name: 'JsonWebTokenError',
+      message: 'jwt is not valid'
+    };
+
+    return res.status(401).json(err);
+  }
 });
 
 app.post('/track/stop', async (req, res) => {
-  const { id_cli } = req.body;
+  const token = req.headers['token'];
 
-  const result = await prisma.work_time.update({
-    where: {
-      id: id_cli
-    },
-    data: {
-      end_time: new Date().toISOString(),
-      updated_at: new Date().toISOString()
+  try {
+    var decoded = jwt.verify(token, cfg.jwt.secret, {
+      ignoreExpiration: false
+    });
+
+    if (decoded) {
+      const id = jwt.decode(token, { complete: true });
+
+      const verify_user = await prisma.user.findFirst({
+        where: {
+          id: id.payload.id
+        }
+      });
+
+      if (verify_user) {
+        const verify_work = await prisma.work_time.findFirst({
+          where: {
+            user_id: id.payload.id,
+            end_date: null
+          }
+        });
+
+        if (verify_work !== null) {
+          const result = await prisma.work_time.update({
+            where: {
+              id: verify_work.id
+            },
+            data: {
+              end_date: new Date().toISOString(),
+              updated_at: new Date().toISOString()
+            }
+          });
+
+          return res.status(200).json(result);
+        }
+
+        return res.status(200).json({ message: 'Нет незаконченных работ!' });
+      }
     }
-  });
 
-  res.json(result);
+    return res.status(401).json();
+  } catch (err) {
+    err = {
+      name: 'JsonWebTokenError',
+      message: 'jwt is not valid'
+    };
+
+    return res.status(401).json(err);
+  }
+});
+
+app.post('/track/status', async (req, res) => {
+  const token = req.headers['token'];
+
+  try {
+    var decoded = jwt.verify(token, cfg.jwt.secret, {
+      ignoreExpiration: false
+    });
+
+    if (decoded) {
+      const id = jwt.decode(token, { complete: true });
+
+      const verify_user = await prisma.user.findFirst({
+        where: {
+          id: id.payload.id
+        }
+      });
+
+      if (verify_user) {
+        const verify_work = await prisma.work_time.findFirst({
+          where: {
+            user_id: id.payload.id,
+            end_date: null
+          }
+        });
+
+        if (verify_work) {
+          return res.status(200).json(verify_work);
+        }
+
+        return res.status(200).json();
+      }
+    }
+
+    return res.status(401).json();
+  } catch (err) {
+    err = {
+      name: 'JsonWebTokenError',
+      message: 'jwt is not valid'
+    };
+
+    return res.status(401).json(err);
+  }
+});
+
+app.post('/track/list', async (req, res) => {
+  const token = req.headers['token'];
+
+  try {
+    var decoded = jwt.verify(token, cfg.jwt.secret, {
+      ignoreExpiration: false
+    });
+
+    if (decoded) {
+      const id = jwt.decode(token, { complete: true });
+
+      const verify_user = await prisma.user.findFirst({
+        where: {
+          id: id.payload.id
+        }
+      });
+
+      if (verify_user) {
+        const verify_work = await prisma.work_time.findMany({
+          where: {
+            user_id: id.payload.id
+          }
+        });
+
+        return res.status(200).json(verify_work);
+      }
+    }
+
+    return res.status(401).json();
+  } catch (err) {
+    err = {
+      name: 'JsonWebTokenError',
+      message: 'jwt is not valid'
+    };
+
+    return res.status(401).json(err);
+  }
 });
 
 app.post('/auth/signup', async (req, res) => {
   const { login, password } = req.body;
-  var salt = await bcrypt.genSalt(10);
+  let salt = await bcrypt.genSalt(10);
 
   const result = await prisma.user.create({
     data: {
@@ -86,42 +248,44 @@ app.post('/auth/signin', async (req, res) => {
 });
 
 app.post('/auth/connect', async (req, res) => {
-  const { token } = req.body;
+  const token = req.headers['token'];
 
   try {
     var decoded = jwt.verify(token, cfg.jwt.secret, {
       ignoreExpiration: false
     });
+
+    if (decoded) {
+      const id = jwt.decode(token, { complete: true });
+
+      const result = await prisma.user.findFirst({
+        where: {
+          id: id.payload.id
+        }
+      });
+
+      if (result) {
+        return res.status(200).json({
+          jwt: jwt.sign(
+            {
+              id: result.id
+            },
+            cfg.jwt.secret,
+            { expiresIn: cfg.jwt.end_time }
+          )
+        });
+      }
+    }
+
+    return res.status(401).json();
   } catch (err) {
     err = {
       name: 'JsonWebTokenError',
       message: 'jwt is not valid'
     };
+
+    return res.status(401).json(err);
   }
-
-  if (decoded) {
-    const id = jwt.decode(token, { complete: true });
-
-    const result = await prisma.user.findFirst({
-      where: {
-        id: id.payload.id
-      }
-    });
-
-    if (result) {
-      return res.status(200).json({
-        jwt: jwt.sign(
-          {
-            id: result.id
-          },
-          cfg.jwt.secret,
-          { expiresIn: cfg.jwt.end_time }
-        )
-      });
-    }
-  }
-
-  return;
 });
 
 app.listen(cfg.server.port, () => {
